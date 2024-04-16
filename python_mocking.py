e4c8eefe3072c21295afbb07536171d44f2a0ccb
Gopinath <45130413+gopi-123@users.noreply.github.com>
2024-04-16 02:01:21 +0200

    Update python_mocking.py

--- python_mocking.py
+++ python_mocking.py
@@ -295,7 +295,7 @@ This approach simplifies the test case by directly mocking the entire function, 
 
 
 
-################################### solution 5 below  VVV BEsT#######################
+################################### solution 5 below  VVV Best #######################
 
 
 
@@ -448,5 +448,44 @@ Adjust the patching target (`'my_module.elasticsearch.Elasticsearch'`) based on 
 
 
 
-###################33
+###################  Final Solution #####################
+# my_module.py
+def add(a, b):
+    print("Hello222")
+    return a + b
 
+
++++++++++++
+# Python_mocking/example_adding/test_add.py
+#test_add.py
+
+import unittest
+from unittest.mock import patch, MagicMock
+import my_module  # Import the module containing the add function
+
+class TestAddFunction(unittest.TestCase):
+    @patch('my_module.add', MagicMock(return_value=5))
+    def test_add_function(self):
+        # Call the add function (the patched version, not the actual add function)
+        result = my_module.add(2, 3)
+
+        # Assert that the result matches the expected mock return value
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
+
+"""
+In this approach:
+
+We import the my_module module containing the add function.
+We use the @patch decorator directly on the import statement to patch the add function with a MagicMock object.
+We configure the MagicMock object to return the desired mock return value (5) when called.
+Inside the test case method, we call my_module.add, which is now replaced by the MagicMock object.
+Since the MagicMock object is configured to return 5, the result of calling my_module.add(2, 3) will be 5.
+We then assert that the result matches the expected mock return value (5).
+This ensures that the original add function is completely replaced by the MagicMock object, and its implementation is not executed at all during the test.
+
+"""
+
+#################
